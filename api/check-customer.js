aeb9df7d253d5410c6928c5f3f9d36e06554905b
Colibrisale <kisvaska478@gmail.com>
2025-02-27 18:16:26 +0100

    Update check-customer.js

--- api/check-customer.js
+++ api/check-customer.js
@@ -28,5 +28,5 @@ app.get("/api/check-customer", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+// 🔹 Добавляем экспорт
+export default app;
